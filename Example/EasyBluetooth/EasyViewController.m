40ae9efaea30306967f02bef9ea7f05a0eddb462
YuIan <mysevenmi2013@gmail.com>
2018-04-11 09:05:53 +0800

    update

---
+++ Example/EasyBluetooth/EasyViewController.m
@@ -0,0 +1,29 @@
+//
+//  EasyViewController.m
+//  EasyBluetooth
+//
+//  Created by mysevenmi2013@gmail.com on 04/09/2018.
+//  Copyright (c) 2018 mysevenmi2013@gmail.com. All rights reserved.
+//
+
+#import "EasyViewController.h"
+
+@interface EasyViewController ()
+
+@end
+
+@implementation EasyViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
